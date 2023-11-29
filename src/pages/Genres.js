ab1c8a6a0befe7fdb8be5d881777ab028f1c5301
alexsylvian <lazeratus1@gmail.com>
2023-11-28 21:22:46 -0500

    added genres and artists routes

---
+++ src/pages/Genres.js
@@ -0,0 +1,18 @@
+import NavBar from "../components/NavBar";
+
+function Genres() {
+    
+
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>Genres Page</h1>
+            </main>
+        </>
+    )
+}
+
+export default Genres
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import Home from "./pages/Home";
+import Artists from "./pages/Artists";
+import Genres from "./pages/Genres";
 
 const routes = [
     {
       path: "/",
       element: <Home />,
+    //   errorElement: <ErrorPage />
+    },
+    {
+      path: "/artists",
+      element: <Artists />,
+    //   errorElement: <ErrorPage />
+    },
+    {
+      path: "/genres",
+      element: <Genres />,
     //   errorElement: <ErrorPage />
     }
 ]
