b3edd059cd3dfbf70eaece4bfe47e2bc4fbb8899
alexsylvian <lazeratus1@gmail.com>
2023-11-28 21:38:44 -0500

    added artists and genres functionality

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -2,27 +2,27 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
 function Artists() {
-//   const [artists, setArtists] = useState([])
+  const [artists, setArtists] = useState([])
 
-//   useEffect(() =>{
-//     fetch("http://localhost:4000/artists")
-//       .then(r => r.json())
-//       .then(data => setArtists(data))
-//       .catch(error => console.error(error))
-//   }, [])
+  useEffect(() =>{
+    fetch("http://localhost:4001/artists")
+      .then(r => r.json())
+      .then(data => setArtists(data))
+      .catch(error => console.error(error))
+  }, [])
 
-//   const directorList = artists.map(director =>{
-//     return <article
-//     key={director.id}
-//     >
-//       <h2>{director.name}</h2>
-//       <ul>
-//       {director.movies.map(movie => (
-//         <li>{movie}</li>
-//       ))}
-//     </ul>
-//     </article>
-//   });
+  const artistList = artists.map(artist =>{
+    return <article
+    key={artist.id}
+    >
+      <h2>{artist.name}</h2>
+      <ul>
+      {artist.songs.map(song => (
+        <li>{song}</li>
+      ))}
+    </ul>
+    </article>
+  });
 
   return (
     <>
@@ -31,7 +31,7 @@ function Artists() {
       </header>
       <main>
         <h1>Artists Page</h1>
-        {/* {directorList} */}
+        {artistList}
       </main>
     </>
   );
--- src/pages/Genres.js
+++ src/pages/Genres.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
 function Genres() {
-    
+    const [genres, setGenres] = useState([])
+
+    useEffect(() =>{
+      fetch("http://localhost:4001/genres")
+        .then(r => r.json())
+        .then(data => setGenres(data))
+        .catch(error => console.error(error))
+    }, [])
+  
+    const genreList = genres.map(genre =>{
+      return <article
+      key={genre.id}
+      >
+        <h2>{genre.name}</h2>
+        <ul>
+        {genre.songs.map(song => (
+          <li>{song}</li>
+        ))}
+      </ul>
+      </article>
+    });
 
     return(
         <>
@@ -10,6 +31,7 @@ function Genres() {
             </header>
             <main>
                 <h1>Genres Page</h1>
+                {genreList}
             </main>
         </>
     )
