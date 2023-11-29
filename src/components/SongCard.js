a29284282ef0856807270f31ca9d8cc5bf36ae34
alexsylvian <lazeratus1@gmail.com>
2023-11-28 21:27:33 -0500

    updated SongCard

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 
-function SongCard(){
+function SongCard({ title, id }){
     return(
-        <div>
-            <h1>hi</h1>
-        </div>
+        <article>
+            <h2>{title}</h2>
+            <Link to={`/song/${id}`}>View Song Info</Link>
+        </article>
     )
 }
 
 export default SongCard
+
+
+// import { Link } from 'react-router-dom';
+
+// // function MovieCard({ title, id }) {
+// //   return (
+// //     <article>
+// //       <h2>{title}</h2>
+// //       <Link to={`/movie/${id}`}>View Info</Link>
+// //     </article>
+// //   );
+// // }
+
+// // export default MovieCard;
